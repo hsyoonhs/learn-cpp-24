02badf182b43d41e82bd9ee616ea9014deacdb4b
Yoon Hyeseong <80237040+hsyoonhs@users.noreply.github.com>
2024-03-18 16:41:19 +0900

    Add files via upload

---
+++ 0318/0318_01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// void play(int tries, string answer, int length, string prob);
+
+class Fortune
+{
+    public:
+        void play()
+        {
+            cout << "test" << endl;
+        }
+};
+
+int main()
+{
+    Fortune game;
+    game.play();
+}
